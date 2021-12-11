fec98f712fd100f5a0dfb8214ad0cb87d581d923
Serena Watson <48436940+serenawatson@users.noreply.github.com>
2021-12-11 13:34:14 +1100

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,9 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import pandas as pd
+from datetime import date
+from openpyxl import load_workbook
 
 ##### Comments #####
 
@@ -16,28 +19,53 @@ from selenium.webdriver.common.by import By
 
 ##### Code #####
 
-#stonks list
-stocks = ["CBA.AX", "NAB.AX", "WBC.AX", "ANZ.AX"]
+#read in list of stocks
+df = pd.read_excel("stocks.xls")
+
+#set up list with date
+data = []
+today = date.today()
+data.append(today)
+
+#set up service
+service = Service("/Users/serena/Downloads/chromedriver")
+service.start()
 
 #for each stonk
-for stock in stocks:
+for stock in df["stocks"]:
     
     # Use chrome webdriver
-    driver = webdriver.Chrome("/Users/serena/Downloads/chromedriver")
+    driver = webdriver.Remote(service.service_url)
     
     # Get website
     yahoo = driver.get("https://au.finance.yahoo.com/")
     
     # Find search bar and enter a text
-    search = driver.find_element_by_id('yfin-usr-qry')
+    search = driver.find_element(By.ID, 'yfin-usr-qry')
     search.send_keys("{0}".format(stock))
     time.sleep(5)
     pg = search.send_keys(Keys.RETURN)
     time.sleep(7)
     
     #extract previous closing value
-    prev_close = driver.find_element_by_xpath("//*[@id='quote-header-info']/div[3]/div[1]/div/fin-streamer[1]").get_attribute("innerHTML")
+    prev_close = driver.find_element(By.XPATH, "//*[@id='quote-summary']/div[1]/table/tbody/tr[1]/td[2]").get_attribute("innerHTML")
     
-    #print value
-    print(prev_close)
+    #appending to list
+    data.append(prev_close)
+    driver.close()
+ 
+driver.quit()
+
+#writing to excel
+
+#pre-existing excel book
+name = 'prev_closing.xlsx'
+wb = load_workbook(name)
+page = wb.active
+
+#appending new row with the list of data values:
+page.append(data)
+
+wb.save(filename=name)
+
 
