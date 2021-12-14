013f9862854039bd4f6b652f74f7ec28a62ae693
rebeccah-dotcom <rbeccahsu@gmail.com>
2021-12-14 16:25:42 +1100

    Attempted separating stocks by sheet

--- output.py
+++ output.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 from datetime import date
-from openpyxl import load_workbook
+from openpyxl import load_workbook 
 
 
 #### CODE ####
@@ -23,7 +23,7 @@ df = pd.read_excel("stocks.xls")
 # this time data is a list of lists
 data = []
 
-service = Service("/Users/serena/Downloads/chromedriver")
+service = Service("/Users/rebec/Downloads/chromedriver")
 service.start()
 
 for stock in df["stocks"]:
@@ -53,7 +53,6 @@ for stock in df["stocks"]:
     
 driver.quit()
 
-
 #writing to excel
 
 #pre-existing excel book
@@ -63,7 +62,29 @@ page = wb.active
 
 # appending new row with the list of data values for each stock
 for stock in data:
+    print(stock)
     page.append(stock)
+
+# create 4 worksheets for each stock with headings in each
+if len(wb.sheetnames) != len(df["stocks"]) + 1:
+    for stock in df["stocks"]:
+        ws = wb.create_sheet(stock)
+        ws['A1'] = 'date'
+        ws['B1'] = 'stock_id'
+        ws['C1'] = 'prev_close'
+        ws['D1'] = 'prev_open' 
+        ws['E1'] = 'change'
+
+
+# appending new row with the list of data values for each stock
+n = 1
+sheets = wb.sheetnames
+zip_data = zip(wb, data)
+for sheet, stock in zip_data:
+    ws = wb[sheets[n]]
+    print(stock)
+    ws.append(stock)
+    n += 1
 
 wb.save(filename=name)
 
